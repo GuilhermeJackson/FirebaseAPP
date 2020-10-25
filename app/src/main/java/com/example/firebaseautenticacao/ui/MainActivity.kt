d57cf3b3741c6f2f702f38ac4c98e98e4c61c9bb
Guilherme Lamim <guilherme.lamim96@gmail.com>
2020-10-25 13:57:13 -0300

    Realizando login com aconta do Google

--- app/src/main/java/com/example/firebaseautenticacao/ui/MainActivity.kt
+++ app/src/main/java/com/example/firebaseautenticacao/ui/MainActivity.kt
@@ -5,6 +5,11 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firebaseautenticacao.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import com.example.firebaseautenticacao.ui.cadastrar.CadastrarActivity as CadastrarActivity1
@@ -23,6 +28,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        validateLoginUser()
+        registerUser()
+
+        cardView_LoginGoogle.setOnClickListener{
+            serviceGoogle()
+        }
+
+
+    }
+    private fun validateLoginUser() {
         button_login.setOnClickListener {
             val user = auth.currentUser
             if (user == null) {
@@ -33,12 +48,46 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent2)
             }
         }
+    }
 
+    private fun registerUser(){
         button_cadastrar.setOnClickListener {
             intent = Intent(this, CadastrarActivity1::class.java)
             startActivity(intent)
         }
     }
+
+    private fun serviceGoogle(){
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if(account == null) {
+            val intent = googleSignInClient.signInIntent
+            startActivityForResult(intent, 555)
+        } else {
+            Toast.makeText(this, "Já esta logado", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, PrincipalActivity::class.java))
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 555) {
+            val task= GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.result
+                startActivity(Intent(this, PrincipalActivity::class.java))
+            } catch (e: ApiException){
+                Toast.makeText(this, "Erro ao logar com a conta do Google", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
 
     override fun onStart() {
         super.onStart()
